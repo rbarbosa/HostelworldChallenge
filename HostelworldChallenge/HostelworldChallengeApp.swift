d0d29f292ac1ed7383659d904affc60fdc5fded0
Rui Barbosa <rui.barbosa@hey.com>
2024-12-04 14:55:11 +0000

    Initial Commit

---
+++ HostelworldChallenge/HostelworldChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostelworldChallengeApp.swift
+//  HostelworldChallenge
+//
+//  Created by Rui Barbosa on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HostelworldChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
