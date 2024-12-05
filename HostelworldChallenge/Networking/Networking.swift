667367a6397f08e0306a4932c28e5b142542a34c
Rui Barbosa <rui.barbosa@hey.com>
2024-12-05 07:29:47 +0000

    Move Query to its own file
    Signed-off-by: Rui Barbosa <rui.barbosa@hey.com>

--- HostelworldChallenge/Networking/Networking.swift
+++ HostelworldChallenge/Networking/Networking.swift
@@ -65,21 +65,3 @@ final class Networking {
         return urlComponents.url
     }
 }
-
-
-// MARK: - Query type
-
-enum QueryType {
-    case cityProperties(cityId: String)
-    case property(propertyId: String)
-
-    var path: String {
-        switch self {
-        case .cityProperties(let cityId): 
-            "cities/\(cityId)/properties"
-        
-        case .property(let propertyId):
-            "properties/\(propertyId)"
-        }
-    }
-}
